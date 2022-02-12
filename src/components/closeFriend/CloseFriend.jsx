8a48172e6033012938461c369f38f6f4c8630f70
Mayank Joshi <mayank2000j@gmail.com>
2022-02-12 12:14:27 +0530

    Design Using ReactJs

---
+++ src/components/closeFriend/CloseFriend.jsx
@@ -0,0 +1,10 @@
+import "./CloseFriend.css";
+
+export default function CloseFriend({user}) {
+  return (
+    <li className="sidebarFriend">
+      <img className="sidebarFriendImg" src={user.profilePicture} alt="" />
+      <span className="sidebarFriendName">{user.username}</span>
+    </li>
+  );
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Topbar.css";
+// import AllInclusiveIcon from '@mui/icons-material/AllInclusive';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ChatIcon from '@mui/icons-material/Chat';
+import Person from '@mui/icons-material/Person';
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Metaverse</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <ChatIcon />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <NotificationsIcon />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+      </div>
+    </div>
+  );
+}
