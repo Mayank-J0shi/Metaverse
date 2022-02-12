310a6837357534f38d2a39b65174a8c52ec79cc2
Mayank Joshi <mayank2000j@gmail.com>
2022-02-13 03:47:31 +0530

    added apiCalls.js and context api but not completly implememted

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,14 @@
 import "./login.css";
+import { useRef } from "react";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -10,16 +18,29 @@ export default function Login() {
             Come into the meta world around you on Metaverse.
           </span>
         </div>
-        <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
+        <div className="loginRight" onSubmit={handleClick}>
+          <form className="loginBox">
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              required
+              minLength={6}
+              type="password"
+              className="loginInput"
+              ref={password}
+            />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -5,14 +5,16 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import axios from "axios";
+import { useParams } from "react-router";
 
 export default function Profile() {
   const PF=process.env.REACT_APP_PUBLIC_FOLDER;
   const [user,setUser]=useState({});
+  const username = useParams().username;
 
   useEffect(()=>{
     const fetchUser = async ()=>{
-      const res = await axios.get(`/users?username=divya`);
+      const res = await axios.get(`/users?username=${username}`);
       setUser(res.data);
     }
     fetchUser();
@@ -43,7 +45,7 @@ export default function Profile() {
             </div>
           </div>
           <div className="profileRightBottom">
-            <Feed username="divya"/>
+            <Feed username={username}/>
             <Rightbar user={user}/>
           </div>
         </div>
