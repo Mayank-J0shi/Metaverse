0f93ee51808ea34d089973960fdadff2c18b529d
Mayank Joshi <mayank2000j@gmail.com>
2022-02-24 20:28:18 +0530

    Completed the private conversation part

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -17,11 +17,25 @@ export default function Messenger() {
   const socket = useRef();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState([]);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
   const scrollRef = useRef();
 
   useEffect(() => {
     socket.current = io("ws://localhost:8900");
+    socket.current.on("getMessage", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now(),
+      });
+    });
   }, []);
+
+  useEffect(() => {
+    arrivalMessage &&
+      currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage, currentChat]);
 
   useEffect(() => {
     //client to socket server
@@ -65,6 +79,17 @@ export default function Messenger() {
       text: newMessage,
       conversationId: currentChat._id,
     };
+
+    const receiverId = currentChat.members.find(
+      (member) => member !== user._id
+    );
+
+    socket.current.emit("sendMessage", {
+      senderId: user._id,
+      receiverId,
+      text: newMessage,
+    });
+
     try {
       const res = await axios.post("/messages", message);
       setMessages([...messages, res.data]);
--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,3 @@
-const { Socket } = require("socket.io");
-
 const io = require("socket.io")(8900, {
   cors: {
     origin: "http://localhost:3000",
@@ -17,17 +15,33 @@ const removeUser = (socketId) => {
   users = users.filter((user) => user.socketId !== socketId);
 };
 
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  //when connect
+  console.log("a user connected.");
+
   //take userId and socketId from user
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
     io.emit("getUsers", users);
   });
-  socket.on("disconnect",()=>{
-      console.log("a user has disconnected");
-      removeUser(socket.id);
-      io.emit("getUsers", users);
-  })
 
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 });
